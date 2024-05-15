dd2f78a1386be4be235d56d1d7eb4610c9404c0c
Ana Carolina Cruz <ana.cruz@ab-inbev.com>
2024-05-15 13:49:13 -0300

    Calcultor app challenge

---
+++ level-intermediate/calculator-app/script.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll("button")
+const display = document.querySelector("#display")
+
+const equation = (event) => {
+  const buttonLabel = event.currentTarget.innerText
+  
+  if ( buttonLabel === 'DEL') {
+    display.value = display.value.substr(0, display.value.length - 1)
+  }  else if ( buttonLabel === 'RESET') {
+    display.value = ""
+  } else if (buttonLabel === '=') {
+    display.value = eval(display.value.toString())
+  } else {
+    display.value += buttonLabel
+  }
+}
+
+buttons.forEach((button) => {
+  button.addEventListener('click', equation)
+})
+______________________
+
