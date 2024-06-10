729b3e8b028f1f8093bd0502e39d1d209c49eebf
Ana Carolina Cruz <ana.cruz@ab-inbev.com>
2024-06-10 15:02:05 -0300

    fix: update calculator functionalities

--- level-intermediate/calculator-app/script.js
+++ level-intermediate/calculator-app/script.js
@@ -1,20 +1,112 @@
 const buttons = document.querySelectorAll("button")
 const display = document.querySelector("#display")
+let calculated = false
 
-const equation = (event) => {
-  const buttonLabel = event.currentTarget.innerText
+const inputScroll = () => {
+  display.scrollLeft = display.scrollWidth;
+}
+
+const math = () => {
+  const operators = ['+', '-', '*', '/']
+  const operator = operators.find(op => display.value.includes(op))
+
+  if (!operator) return {}
   
-  if (buttonLabel === 'DEL') {
-    display.value = display.value.substr(0, display.value.length - 1)
-  }  else if ( buttonLabel === 'RESET') {
-    display.value = ""
-  } else if (buttonLabel === '=') {
-    display.value = eval(display.value.toString())
-  } else {
-    display.value += buttonLabel
+  inputScroll()
+
+  const splitTest = display.value.split(operator)
+
+  const equation = splitTest.reduce((first, second) => {
+    const firstInt = +first
+    const secondInt = +second
+
+
+    if (operator === '+') {
+      return firstInt + secondInt
+    } else if (operator === '-') {
+      return firstInt - secondInt
+    } else if (operator === '*') {
+      return firstInt * secondInt
+    } else if (operator === '/') {
+      if (secondInt === 0) {
+        return "Can't divide by 0"
+      } else {
+        return firstInt / secondInt
+      }
+    }
+  })
+
+  display.value = equation
+  calculated = true
+}
+
+const keyboardClick = (keyPressed) => {
+  keyPressed.preventDefault();
+  const { key } = keyPressed;
+  inputScroll()
+
+  if (calculated) {
+    display.value = ''
+    calculated = false
   }
+
+  const keyboardLabelObj = {
+    0: () => {display.value += 0},
+    1: () => {display.value += 1},
+    2: () => {display.value += 2},
+    3: () => {display.value += 3},
+    4: () => {display.value += 4},
+    5: () => {display.value += 5},
+    6: () => {display.value += 6},
+    7: () => {display.value += 7},
+    8: () => {display.value += 8},
+    9: () => {display.value += 9},
+    '/': () => {display.value += '/'},
+    '*': () => {display.value += '*'},
+    '-': () => {display.value += '-'},
+    '+': () => {display.value += '+'},
+    '.': () => {display.value += '.'},
+    Enter: () => {math() ?? ''},
+    Backspace: () => {display.value = display.value.substr(0, display.value.length - 1) ?? ''},
+    Delete: () => {display.value = ''}
+  }
+
+  keyboardLabelObj[key]?.()
+};
+
+const btnClick = (event) => {
+  const buttonLabel = event.currentTarget.innerText
+  inputScroll()
+
+  if (calculated) {
+    display.value = ''
+    calculated = false
+  }
+
+  const btnLabelObj = {
+    0: () => {display.value += 0},
+    1: () => {display.value += 1},
+    2: () => {display.value += 2},
+    3: () => {display.value += 3},
+    4: () => {display.value += 4},
+    5: () => {display.value += 5},
+    6: () => {display.value += 6},
+    7: () => {display.value += 7},
+    8: () => {display.value += 8},
+    9: () => {display.value += 9},
+    '/': () => {display.value += '/'},
+    '*': () => {display.value += '*'},
+    '-': () => {display.value += '-'},
+    '+': () => {display.value += '+'},
+    '.': () => {display.value += '.'},
+    '=': () => {math() ?? ''},
+    'DEL': () => {display.value = display.value.substr(0, display.value.length - 1) ?? ''},
+    'RESET': () => {display.value = ''},
+  }
+
+  btnLabelObj[buttonLabel]?.()
 }
 
 buttons.forEach((button) => {
-  button.addEventListener('click', equation)
+  button.addEventListener('click', btnClick)
 })
